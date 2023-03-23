bc6e1097a6e43e563f9d5cf5d2578ed9848c4188
jasminecd <50221241+jasminecd@users.noreply.github.com>
2023-03-23 14:27:19 -0700

    Updates

---
+++ precache-manifest.82e0c4ffcd13ee8a57c3e3dc96ecc6e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce5f29db5de2213787ab400cf36c8da",
+    "url": "./index.html"
+  },
+  {
+    "revision": "25c87d5a322acf1890eb",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "13dc5b51c0c060f78b6d",
+    "url": "./static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "25c87d5a322acf1890eb",
+    "url": "./static/js/2.47b74c69.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.47b74c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13dc5b51c0c060f78b6d",
+    "url": "./static/js/main.4442029e.chunk.js"
+  },
+  {
+    "revision": "7df2b448bc79c08da260",
+    "url": "./static/js/runtime-main.2175a412.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jasmine-portfolio/precache-manifest.364d423df6c2f15ae3722c9946ba3392.js"
+  "./precache-manifest.82e0c4ffcd13ee8a57c3e3dc96ecc6e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jasmine-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
